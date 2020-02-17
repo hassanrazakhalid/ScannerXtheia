f332030573a662f4897c4ab46f0900334f4019f5
Hassan Raza Khalid <hassanrazakhalid@yahoo..com>
2020-02-17 17:53:11 +0500

    First push

---
+++ Samples/Scanner-framework/Scanner_framework.h
@@ -0,0 +1,19 @@
+//
+//  Scanner_framework.h
+//  Scanner-framework
+//
+//  Created by hassan on 2/17/20.
+//  Copyright © 2020 Occipital. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Scanner_framework.
+FOUNDATION_EXPORT double Scanner_frameworkVersionNumber;
+
+//! Project version string for Scanner_framework.
+FOUNDATION_EXPORT const unsigned char Scanner_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Scanner_framework/PublicHeader.h>
+#import "ViewController.h"
+
